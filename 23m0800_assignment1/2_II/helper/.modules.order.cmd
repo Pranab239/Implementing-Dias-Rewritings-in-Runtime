86121e99adb9d722cf274effd944220a873a9ae9
Pranab Paul <pranab4752@gmail.com>
2024-05-25 11:24:13 +0530

    Added dias rules

--- 23m0800_assignment1/2_II/helper/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/cs695/23m0800_assignment1/2_II/helper/modules.order := {   echo /home/cs695/23m0800_assignment1/2_II/helper/help_dr_doom.ko; :; } > /home/cs695/23m0800_assignment1/2_II/helper/modules.order
